4fd744aee05c9557bb97d12f4aa8e9ae9f27179e
ibrahimtrahim <trahimbrahim@gmail.com>
2024-06-27 16:17:04 +0000

    Idokan landing page

---
+++ src/sections/SpecialsOffers.jsx
@@ -0,0 +1,47 @@
+import { arrowRight } from "../assets/icons";
+import { offer } from "../assets/images";
+import { Button } from "../components";
+
+const SpecialOffer = () => {
+  return (
+    <section className='flex justify-between items-center max-xl:flex-col-reverse gap-10 max-container'>
+      <div className='flex-1'>
+        <img
+          src={offer}
+          alt='Shoe Promotion'
+          width={773}
+          height={687}
+          className='object-contain w-full'
+        />
+      </div>
+      <div className='flex flex-1 flex-col'>
+        <h2 className='text-4xl font-palanquin font-bold'>
+          <span className='text-coral-red'>Special </span>
+          Offer
+        </h2>
+        <p className='mt-4 info-text'>
+          Step into Moroccan heritage with Idokan shoes, where every pair offers unparalleled craftsmanship 
+          and comfort. Discover the perfect blend of tradition and modernity, making each stride a journey 
+          through authentic elegance.
+        </p>
+        <p className='mt-6 info-text'>
+          Explore a world of refined craftsmanship with Idokan shoes, designed 
+          to exceed your expectations with unmatched quality and elegance. 
+          Your journey with Idokan is a step into timeless tradition, offering an 
+          experience that is nothing short of exceptional.
+        </p>
+        <div className='mt-11 flex flex-wrap gap-4'>
+          <Button label='Shop now' iconURL={arrowRight} />
+          <Button
+            label='Learn more'
+            backgroundColor='bg-white'
+            borderColor='border-slate-gray'
+            textColor='text-slate-gray'
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SpecialOffer;
